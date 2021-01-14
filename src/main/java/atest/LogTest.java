3831ea05226054c357cc3fc26c59f0e928d23993
Hong Li <holiholiholiholi@gmail.com>
2021-01-14 13:38:24 +0100

    add logger

---
+++ src/main/java/atest/LogTest.java
@@ -0,0 +1,11 @@
+package atest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogTest {
+    public static void main(String args[]){
+        System.out.println("sdfs");
+      log.info("test log..");
+    }
+}
